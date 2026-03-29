a02494739acec9552a854a983877ab9f0027d500
Lup Yuen Lee <luppy@appkaki.com>
2026-03-29 22:26:53 +0800

    Sleep less
    Signed-off-by: Lup Yuen Lee <luppy@appkaki.com>

--- download-github-logs.sh
+++ download-github-logs.sh
@@ -70,7 +70,7 @@ function ingest_log {
   set +x ; echo job_id=$job_id ; set -x
   if [[ "$job_id" == "" ]]; then
     set +x ; echo "**** Job ID missing for Run ID $run_id, Job Name $job_name" ; set -x
-    sleep 10
+    sleep 01
     return
   fi
   sleep 1
